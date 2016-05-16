c25a29d0b2512fea3162b7d205e8cc94424b8219
AustinWentz <austin@austinwentz.com>
2016-05-16 15:52:56 -0500

    Updated delete functionality

--- delete.py
+++ delete.py
@@ -1,8 +1,12 @@
 import os
 
-employees = ["AC" , "CJ", "CM", "DA", "DF", "JD", "JL", "KJ", "RR", "SC", "TS", "TR", "TK"]
+employees = ["AC" , "CJ", "CM", "DA", "DF", "JD", "JL", "JM", "RR", "SC", "TS", "TR", "TK"]
 
 for employee in employees:
+	os.system("rm " + employee + ".txt")
 	os.system("rm " + employee + "_report.txt")
+
+for day in range(0,32):
+	os.system("rm day" + str(day) + ".txt")
 
 os.system("rm Monthly_report.txt")
